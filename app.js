d5159bce0bbcb6183e8b935e93aa46ef6f9d658f
Ivy Nguyen <ivynguyen003@gmail.com>
2018-02-26 12:58:37 -0800

    modifed js & index

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ function onReady() {
     const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit', () => {
+    addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         let title = newToDoText.value;
         let newLi = document.createElement('li');
         let checkbox = document.createElement('input');
         checkbox.type="checkbox";
-        newLi.texContent = title;
+        newLi.textContent = title;
         newLi.appendChild(checkbox);
         toDoList.appendChild(newLi);
         newToDoText.value = '';
