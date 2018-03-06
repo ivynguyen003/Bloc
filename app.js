fc86c51e116a0025278a8f2c81d365efef9fcd25
Ivy Nguyen <ivynguyen003@gmail.com>
2018-03-06 12:22:07 -0800

    Checkpoint 10: completed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function onReady() {
     const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
+    
     addToDoForm.addEventListener('submit', event => {
         event.preventDefault();
         let title = newToDoText.value;
