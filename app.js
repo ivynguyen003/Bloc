5b96987aed97c1c4a334de718f8eef223c6ff705
Ivy Nguyen <ivynguyen003@gmail.com>
2018-03-06 12:15:39 -0800

    testing

--- app.js
+++ app.js
@@ -1,83 +1,27 @@
-function onReady() { 
-  let toDos = JSON.parse(localStorage.getItem('toDos')) || [];// [0,1,2,3,4,5,6,7,8,9,10] index
-  let nextID = toDos.length?toDos[toDos.length].id : 0;// [0,1,2,3,4,5,6,7,8,10,9,10] ID
-  nextID++;
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+function onReady() {
 
-  function createNewToDo(){
-    if (!newToDoText.value){return;}
-    toDos.push({
-        title: newToDoText.value,
-        complete: false, //question: why set this to false?//
-        id:nextID
-       });
-        nextID++;
-        newToDoText.value = '';   
-        renderTheUI();
-      }
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
 
-  function deleteToDo(id){
-    toDos = toDos.filter(item => item.id !== id);
-    renderTheUI();
-    storeToLocal();
-  }
+    addToDoForm.addEventListener('submit', () => {
+        event.preventDefault();
 
-  function toggleComplete (check){
-    check.complete = !check.complete;
-  }
+        let title = newToDoText.value;
+        let newLi = document.createElement('li');
+        let checkbox = document.createElement('input');
 
-  function storeToLocal(){
-    localStorage.setItem('toDos',JSON.stringify(toDos));
-  }//question!
+        checkbox.type="checkbox";
 
-  function renderTheUI(){
-        const toDoList = document.getElementById('toDoList');
-        toDoList.textContent = '';
+        newLi.texContent = title;
+        newLi.appendChild(checkbox);
+        toDoList.appendChild(newLi);
+        
+        newToDoText.value = '';
 
-        toDos.forEach(function(toDo){
-            const newLi = document.createElement('li')
-            
-            const checkbox = document.createElement('input');
-            checkbox.type = "checkbox";//question: corresponding to html type/id?
-                                      // to have same styling in css?
-
-            const deleteItem = document.createElement('button');
-            deleteItem.type = "button";
-
-
-            const title = document.createElement('span');
-            title.textContent = toDo.title;
-
-            newLi.textContent = toDo.title;
-
-            newLi.appendChild(deleteItem);
-            toDoList.appendChild(newLi);
-            newLi.appendChild(checkbox);
-            
-            deleteItem.addEventListener('click', event => {
-            deleteToDo(toDo.id);
-            });
-
-            checkbox.addEventListener('change', event => {
-            toggleComplete(toDo);
-            });
-
-        });
-    }
-
-  addToDoForm.addEventListener('submit', event =>{
-      event.preventDefault();
-      createNewToDo();
-      newToDoText.value = '';
-      renderTheUI();
-      storeToLocal();
-  });
-
-  renderTheUI();
+    });
 }
-
 window.onload = function (){
+    alert("Done Loading!");
     onReady();
 };
